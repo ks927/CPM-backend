229ddfa1644be002fd67bb7adff9bb07dba163d5
KSheehan <ksheehan927@gmail.com>
2018-08-21 18:02:07 -0400

    Separates routes file

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'leaderboard',
-  });
-
-  const queryString = 'SELECT * FROM leaders';
-
-  connection.query(queryString, (err, rows, fields) => {
-    if (err) {
-      console.log('failed to query leaders: ' + err);
-      res.sendStatus(500);
-      res.end();
-      return;
-    }
-    console.log('fetched leaders successfully!');
-
-    res.json(rows);
-  });
-
-  // res.send({
-  //   hi: 'hi there you',
-  // });
-});
+require('./routes/homeRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
---
+++ routes/homeRoutes.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql');
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      database: 'leaderboard',
+    });
+
+    const queryString = 'SELECT * FROM leaders';
+
+    connection.query(queryString, (err, rows, fields) => {
+      if (err) {
+        console.log('failed to query leaders: ' + err);
+        res.sendStatus(500);
+        res.end();
+        return;
+      }
+      console.log('fetched leaders successfully!');
+
+      res.json(rows);
+    });
+  });
+};
